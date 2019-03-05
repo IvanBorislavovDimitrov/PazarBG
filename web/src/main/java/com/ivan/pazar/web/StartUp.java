d40c62ae705fed965e7c2746c33f63c8a4aee9d5
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-03-06 00:03:15 +0200

    Add base layout and some configurations

--- web/src/main/java/com/ivan/pazar/web/StartUp.java
+++ web/src/main/java/com/ivan/pazar/web/StartUp.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.ivan.pazar.persistence"})
 @EntityScan(basePackages = {"com.ivan.pazar.domain"})
-@ComponentScan(basePackages = {"com.ivan.pazar.persistence"})
+@ComponentScan(basePackages = {"com.ivan.pazar.persistence", "com.ivan.pazar.web"})
 public class StartUp {
 
     public static void main(String[] args) {
