9d27f1e9b3347169691b3fd1d53b7fa26ef1afc3
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-03-27 08:11:38 +0200

    Add review poput

---
+++ web/src/main/java/com/ivan/pazar/web/controller/view/review/ReviewEditController.java
@@ -0,0 +1,16 @@
+//package com.ivan.pazar.web.controller.view.review;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class ReviewEditController extends ReviewBaseController {
+//
+//    @GetMapping("/edit")
+//    public ModelAndView editReview(@RequestParam("reviewId") String reviewId, Model model) {
+//
+//    }
+//}
